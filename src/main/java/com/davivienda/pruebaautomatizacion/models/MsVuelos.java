f5ade096f298aa98c60be929a759c1ccb014bc54
Jhon Quinones Arboleda <jhon.quinones@davivienda.com>
2024-10-25 16:45:29 -0500

    (JQA) Test auto

--- src/main/java/com/davivienda/pruebaautomatizacion/models/MsVuelos.java
+++ src/main/java/com/davivienda/pruebaautomatizacion/models/MsVuelos.java
@@ -1,12 +1,24 @@
 package com.davivienda.pruebaautomatizacion.models;
 
-
-import lombok.Data;
-import lombok.Getter;
-
-@Getter
-@Data
 public class MsVuelos {
-    private String Origen;
-    private String Destino;
+
+	private String Origen;
+	private String Destino;
+
+	public String getOrigen() {
+		return Origen;
+	}
+
+	public void setOrigen(String origen) {
+		Origen = origen;
+	}
+
+	public String getDestino() {
+		return Destino;
+	}
+
+	public void setDestino(String destino) {
+		Destino = destino;
+	}
+
 }
